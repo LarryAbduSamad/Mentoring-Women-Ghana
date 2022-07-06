1740efb7d2ab96c8c5696323325ba018375af55d
Abdur Raheem Abdus Samad Larry <samadlarry7@gmail.com>
2022-07-06 18:04:27 +0000

    first deployment of website for Mentoring Women Ghana to see how it looks on the web

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function displayNavItems() {
+    var navItems = document.getElementById("nav");
+    if (navItems.style.display === "block") {
+      navItems.style.display = "none";
+    } else {
+      navItems.style.display = "block";
+    }   
+}
